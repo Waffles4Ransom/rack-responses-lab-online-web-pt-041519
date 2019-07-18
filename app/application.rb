19a12fa2daaca135e03889ceb8918e92e445fc1d
Jenna Ransom <jenna.ransom@gmail.com>
2019-07-18 08:33:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,8 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
-    if Time.now ==
+    time = Time.now.hour
+    if time <= 12
       resp.write "Good Morning"
     else 
       resp.write "Good Afternoon!"
