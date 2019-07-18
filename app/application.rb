e2c1139889633a38f7d66e7add4931a71d9b0753
Jenna Ransom <jenna.ransom@gmail.com>
2019-07-18 08:31:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,19 +3,11 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
-    if Time.now = 
+    if Time.now ==
       resp.write "Good Morning"
     else 
       resp.write "Good Afternoon!"
     end 
-    
-    resp.finish
- 
-    if num_1==num_2 && num_2==num_3
-      resp.write "You Win"
-    else
-      resp.write "You Lose"
-    end
     
     resp.finish
   end
